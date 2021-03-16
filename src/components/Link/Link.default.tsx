3d7bb65510a75ea2729340d282e35574d145e821
mohemohe <mohemohe@ghippos.net>
2021-03-17 03:38:51 +0900

    prevent unnecessary calculations of smartPath

--- src/components/Link/Link.default.tsx
+++ src/components/Link/Link.default.tsx
@@ -1,4 +1,6 @@
+import { isEqual } from 'lodash'
 import * as React from 'react'
+import { useCustomCompareMemo } from 'use-custom-compare'
 import { generateCurvePath, generateRightAnglePath, generateSmartPath, IConfig, ILink, IOnLinkClick, IOnLinkMouseEnter, IOnLinkMouseLeave, IPort, IPosition } from '../../'
 import { ArrowLink, RegularLink } from './variants'
 
@@ -22,7 +24,16 @@ export const LinkDefault = (props: ILinkDefaultProps) => {
   const { config, startPos, endPos, fromPort, toPort, matrix } = props
   const points = config.smartRouting
     ? !!toPort && !!matrix
-      ? generateSmartPath(matrix, startPos, endPos, fromPort, toPort)
+      ? useCustomCompareMemo(
+        () => generateSmartPath(matrix, startPos, endPos, fromPort, toPort),
+        [config, matrix, startPos, endPos, fromPort, toPort],
+        (prevDeps, nextDeps) => {
+          if (nextDeps[0].virtualize === true) {
+            return false
+          }
+          return isEqual(prevDeps, nextDeps)
+        },
+      )
       : generateRightAnglePath(startPos, endPos)
     : generateCurvePath(startPos, endPos)
 
